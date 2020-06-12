939004af3d7a8808dc61b3b3c36e0f5ec34dea0a
Derry <derry@woobiz.id>
2020-06-12 11:22:05 +0700

    face recognition app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Clarifai from "clarifai";
+import ImageSearchForm from "./Components/ImageSearchForm";
+import FaceDetect from "./Components/FaceDetect";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// You need to add your own API key here from Clarifai.
+const app = new Clarifai.App({
+  apiKey: "b552701ad2ac4b4885b857a2ffd0bab0",
+});
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+      imageUrl: "",
+      box: {},
+    };
+  }
+
+  // this function calculate the facedetect location in the image
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  /* this function display the face-detect box base on the state values */
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value });
+  };
+
+  onSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      // if error exist console.log error
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ImageSearchForm
+          onInputChange={this.onInputChange}
+          onSubmit={this.onSubmit}
+        />
+        <FaceDetect box={this.state.box} imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
 }
-
 export default App;
